ce2b82b15b787c4f534c2b1e93885e9c780c9932
Shovan001 <mondalshovan28701@gmail.com>
2024-11-27 01:05:53 +0530

    add

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import TextInputFormContainer from './Components/TextInputForm/TextInputFormContainer'
+
+function App() {
+  
+
+  return (
+    <div>
+      <TextInputFormContainer/>
+    </div>
+  )
+}
+
+
+export default App
+
