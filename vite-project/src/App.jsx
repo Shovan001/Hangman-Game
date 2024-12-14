f4e0f0b4320a00b1675d0deb6255b5dbe7218c15
Shovan001 <mondalshovan28701@gmail.com>
2024-12-14 21:08:27 +0530

    added context API

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,18 +4,24 @@ import './App.css'
 import StartGame from './pages/StartGame'
 import PlayGame from './pages/PlayGame'
 import Home from './pages/Home'
+import { WordContext } from './context/WordContext'
+import { useState } from 'react'
 
 function App() {
-  
 
+  const [wordList, setWordList] = useState([]);
+  const [word, setWord] = useState('');
+  
   return (
-    <div>
+
+    <WordContext.Provider value={{wordList, setWordList, word, setWord}}>
       <Routes>
         <Route path="/start" element={<StartGame />} />
         <Route path="/play" element={<PlayGame />} />
         <Route path='/' element={<Home />} />
       </Routes>
-    </div>
+      </WordContext.Provider>
+    
   )
 }
 
